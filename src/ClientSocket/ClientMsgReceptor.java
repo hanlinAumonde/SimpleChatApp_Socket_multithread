c5f61ce3cf5d2dbe903ba119eb223cdd73247347
Hanlin Wu <hanlin.wu@etu.utc.fr>
2023-03-29 01:52:30 +0000

    Ajouter les commentaires ClientMsgReceptor.java

--- src/ClientSocket/ClientMsgReceptor.java
+++ src/ClientSocket/ClientMsgReceptor.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.SocketException;
 import java.util.concurrent.CountDownLatch;
 
+//Thread de lecture des messages du client
 
 public class ClientMsgReceptor extends Thread{
     //private Socket ClientSocket;
@@ -23,18 +24,25 @@ public class ClientMsgReceptor extends Thread{
     public void run() {
         String Msg_Receive;
 
-        try {
+        try {  //Continuer à boucler pour lire les messages jusqu'à ce que le message de confirmation de sortie envoyé par le serveur soit reçu
             while (isConnect) {
                 Msg_Receive = inputs.readUTF();
                 if (Msg_Receive.startsWith("\nexit valide")) {
                     isConnect = false;
-                    latch.countDown();
+                    latch.countDown(); 
+                    /**
+                    Le thread de lecture appelle la fonction countDown pour faire passer le compteur de la valeur initiale 1 à 0, de sorte que le thread d'écriture qui appelle await soit déverrouillé et continue à s'exécuter. 
+                    Cela garantit que les deux threads sont fermés en même temps que possible
+                    */
                 } else {
                     System.out.println(Msg_Receive);
                 }
             }
         } catch (SocketException socketException) {
             System.out.println("\nErreur! Serveur perdu! InputStream est sur le point d'être fermé.\nVeuille entrez n'importe quel caractère pour terminer le processus");
+            /**
+            Write Thread utilise un scanner, qui est une lecture bloquante, donc des caractères d'entrée supplémentaires sont nécessaires pour continuer l'exécution et déclencher une exception de socket
+            */
         } catch (IOException ioException) {
             ioException.getStackTrace();
         } finally {
