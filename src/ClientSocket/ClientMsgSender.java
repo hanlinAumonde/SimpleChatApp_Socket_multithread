2867bbc6154345278d19f1f7f0fe945cccbeecea
Hanlin Wu <hanlin.wu@etu.utc.fr>
2023-03-29 02:01:59 +0000

    Ajouter les commentaires ClientMsgSender.java

--- src/ClientSocket/ClientMsgSender.java
+++ src/ClientSocket/ClientMsgSender.java
@@ -6,6 +6,8 @@ import java.net.SocketException;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
+//Thread écriture
+
 public class ClientMsgSender extends Thread {
     private boolean isConnect;
 
@@ -22,7 +24,7 @@ public class ClientMsgSender extends Thread {
     @Override
     public void run() {
         String Msg_Send;
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //lecture bloquante
 
         try {
             while (isConnect) {
@@ -34,15 +36,18 @@ public class ClientMsgSender extends Thread {
                     isConnect = false;
                 }
             }
-            // Wait for the "exit valide" message to be received before sending "exit confirmed"
             try {
-                latch.await();
+                latch.await();  //Attendant le termination du thread lecture
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             /**
             outputs.writeUTF("\nexit confirmed");
             outputs.flush();
+
+            Cette partie est utilisée pour simuler les trois poignées de main lorsque la connexion TCP est fermée, pour s'assurer que le flux de données client est fermé plus tôt que la fin du serveur, et l'utilisation de countDownlatch était à l'origine utilisée pour assurer la progression normale de la poignée de main à trois voies .
+Cependant, des problèmes ont été rencontrés lors du test. La dernière prise de contact lève toujours une IOException côté serveur, ce qui provoque une déconnexion anormale du serveur. Par conséquent, une solution alternative est utilisée, qui consiste à laisser le serveur dormir pendant un certain temps pour assurez-vous que le flux de données client est fermé.
+
              */
         } catch (SocketException socketException) {
             System.out.println("\nOutputStream est sur le point d'être fermé");
@@ -50,7 +55,6 @@ public class ClientMsgSender extends Thread {
             ioException.getStackTrace();
         } finally {
             try {
-
                 outputs.close();
             } catch (IOException ioException) {
                 ioException.getStackTrace();
